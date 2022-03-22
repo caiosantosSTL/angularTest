a26784207befa21188bb5744b207dd364992217c
Caio Santos da Silva <vorreicaio@hotmail.com>
2022-03-22 16:36:54 -0600

    new content

--- src/app/diretivas/diretivas.component.ts
+++ src/app/diretivas/diretivas.component.ts
@@ -9,15 +9,50 @@ export class DiretivasComponent implements OnInit {
 
   public condicao: boolean = true
 
+  public list: Array<{ nome: string }> = [
+    {
+      nome: "Luca"
+    },
+    {
+      nome: "Lara"
+    },
+    {
+      nome: "Tomi"
+    },
+    {
+      nome: "Liza"
+    },
+
+  ]
+
+  public nome : string = "Luca"
+  public valorclass: boolean = true
+
+
   constructor() { }
 
+  public onClickAdd(){
+    this.list.push({nome: "Joo"})
+  }
+  
+  public onClickEventList(event: number){
+    this.list.splice(event, 1)
+  }
+
+  public changeValueClass(){
+    if (this.valorclass == true) {
+      this.valorclass = false
+    }else if(this.valorclass == false){
+      this.valorclass = true
+    }
+  }
 
   ngOnInit(): void {
 
-    setInterval(()=>{
+    setInterval(() => {
       if (this.condicao) {
         this.condicao = false
-      }else{
+      } else {
         this.condicao = true
       }
     }, 2000)
